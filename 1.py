56c1c30360456eadfdd91936e89d064911cc354d
Bagnur <78687087+Bagnur@users.noreply.github.com>
2021-02-08 13:59:10 +0600

    HW

---
+++ 1.py
@@ -0,0 +1,82 @@
+print("hello world")
+
+#this is comment
+
+""" 
+this is comment
+this is comment
+this is comment
+"""
+
+
+carname = "Volvo"
+x=50
+
+x = 5
+y = 10
+print(x +y)
+
+
+int
+str
+float
+
+x=5.5
+x=int(x)
+
+x="hello world"
+print(len(x))
+print(x[0])
+
+True 
+False
+
+print(10*5)
+
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+a = 50
+b = 10
+if a>b:
+	print("Hello World")
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+i = 1
+while i < 6:
+  if i == 3: 
+	break
+  i += 1
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+	continue
+  print(i)
+
+ fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+	continue
+  print(x)
+
+for x in 
+range(6):
+  print(x)
+
+cars.append("Honda")
+
+cars.pop(2)
